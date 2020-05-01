f331c8e0ed722a6c57cc76bdce61aa224b7debf9
JaeggerMeisster <47319792+JaeggerMeisster@users.noreply.github.com>
2020-05-01 03:16:11 +0200

    Added positional argument support, optimizations

---
+++ CommandEngine/Models/CommandModelContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace CommandEngine.Models
+{
+    class CommandModelContext
+    {
+        public CommandModelContext()
+        {
+            this.aliasedProperties = new Dictionary<string, PropertyInfo>();
+            this.positionalProperties = new Dictionary<int, PropertyInfo>();
+        }
+
+        // links aliases to the property info of the type
+        public Dictionary<string, PropertyInfo> aliasedProperties { get; }
+
+        // links and positional index to the property info of the type
+        public Dictionary<int, PropertyInfo> positionalProperties { get; }
+    }
+}
